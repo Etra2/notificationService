de70290a88ea10accab52d52f85edfc96a44f3a8
Etra2 <etra1988@gmail.com>
2025-11-19 15:32:56 +0100

    writing to the h2 database

---
+++ src/main/java/com/service/notificationservice/model/Alert.java
@@ -0,0 +1,52 @@
+package com.service.notificationservice.model;
+
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "alerts")
+public class Alert {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public Alert() {}
+
+    public Alert(String content) {
+        this.content = content;
+    }
+
+    // Gettery i settery
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
+
---
+++ src/main/java/com/service/notificationservice/model/Message.java
@@ -0,0 +1,63 @@
+package com.service.notificationservice.model;
+
+import com.service.notificationservice.model.MessagePriority;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "messages")
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private MessagePriority priority;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public Message() {}
+
+    public Message(String content, MessagePriority priority) {
+        this.content = content;
+        this.priority = priority;
+    }
+
+    // Gettery i settery
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public MessagePriority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(MessagePriority priority) {
+        this.priority = priority;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
+
